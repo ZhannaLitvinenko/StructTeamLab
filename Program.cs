b3a9eca6626566b0d3779a8db4040c0eefd630c7
VadymMytr <42971417+VadymMytr@users.noreply.github.com>
2019-03-31 23:59:24 +0300

    Створено репозиторій для спільної роботи над лабораторною роботою №5 з предмету ПАЛМ, 1 курс, ФОТІУС. Група: Митренко, Вакулінський, Литвиненко. Створено прочитання із файлу, обробка прочитаного, описаний конструктор структури.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StructTeamLab
+{
+    partial class Program
+    {
+        static Student[] ReadData(string fileName)
+        {
+            // TODO   implement this method.
+            // It should read the file whose fileName has been passed and fill 
+            Student[] students = new Student[] { };
+            StreamReader reader = new StreamReader(fileName);
+            for (int i = 0; reader.Peek() > -1; i++)
+            {
+                Array.Resize(ref students, students.Length + 1);
+                students[i] = new Student(reader.ReadLine());                
+            }
+            reader.Close();
+            return students;
+        }
+
+        static void runMenu(Student[] studs)
+        {
+            Console.WriteLine("Ввести номер варiанту для виконання\n(1 - Митренко, 2 - Вакулiнський, 3 - Литвиненко)\nДля виходу натиснiть \"0\"");
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case (1):
+                    //Add methods
+                    break;
+                case (2):
+                    //Add methods
+                    break;
+                case (3):
+                    //Add methods
+                    break;
+                case (0):
+                    System.Environment.Exit(0);
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Student[] studs = ReadData("input.txt");
+            runMenu(studs);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace StructTeamLab
+{
+    struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char mathematicsMark;
+        public char physicsMark;
+        public char informaticsMark;
+        public int scholarship;
+
+        public Student(string lineWithAllData)
+        {
+            while (lineWithAllData.Contains("  "))
+                lineWithAllData = lineWithAllData.Replace("  ", " ");
+
+            string[] data = lineWithAllData.Split();
+            surName = data[0];
+            firstName = data[1];
+            patronymic = data[2];
+            sex = char.Parse(data[3]);
+            dateOfBirth = data[4];
+            mathematicsMark = char.Parse(data[5]);
+            physicsMark = char.Parse(data[6]);
+            informaticsMark = char.Parse(data[7]);
+            scholarship = int.Parse(data[8]);
+        }
+
+
+    }
+}
