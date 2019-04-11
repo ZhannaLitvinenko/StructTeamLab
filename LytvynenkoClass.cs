0431abbade7a154e56f5a459b959180fb286716e
ZhannaLytvynenko <zvlitvinenko01@gmail.com>
2019-04-11 10:54:49 +0300

    Виправила помилку в коді

--- LytvynenkoClass.cs
+++ LytvynenkoClass.cs
@@ -10,13 +10,13 @@ namespace StructTeamLab
     {
         static void LytvynenkoTest(Student[] students)
         {
-            for(int i = 0; i < students.Length; i++)
+            for (int i = 0; i < students.Length; i++)
             {
                 double mathMark = Char.GetNumericValue(students[i].mathematicsMark);
                 double infMark = Char.GetNumericValue(students[i].informaticsMark);
                 double physicsMark = Char.GetNumericValue(students[i].physicsMark);
                 int scholarship = students[i].scholarship;
-                if (!students[i].informaticsMark.Equals('2') && !students[i].mathematicsMark.Equals('2') && !students[i].physicsMark.Equals('2') && scholarship == 0)
+                if (mathMark >= 3 && mathMark <= 5 && infMark >= 3 && infMark <= 5 && physicsMark >= 3 && physicsMark <= 5 && scholarship == 0)
                 {
                     Console.WriteLine("\nСтудент:\n{0} {1} {2}\n", students[i].surName, students[i].firstName, students[i].patronymic);
                 }
